85f00f02a6e2094abb87c004309142a80c476573
Takeru Ohta <phjgt308@gmail.com>
2018-11-11 18:31:13 +0900

    Add `softmax` function

--- src/functions/activation.rs
+++ src/functions/activation.rs
@@ -22,3 +22,36 @@ pub fn relu(x: f64) -> f64 {
         0.0
     }
 }
+
+pub fn softmax(xs: &[f64]) -> Vec<f64> {
+    use std::cmp::Ordering;
+
+    let mut max = 0.0;
+    for &x in xs {
+        if x.partial_cmp(&max) == Some(Ordering::Greater) {
+            max = x;
+        }
+    }
+
+    let mut ys = xs.iter().map(|x| (x - max).exp()).collect::<Vec<_>>();
+    let sum = ys.iter().sum::<f64>();
+    ys.iter_mut().for_each(|y| *y /= sum);
+    ys
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn softmax_works() {
+        assert_eq!(
+            softmax(&[1010.0, 1000.0, 990.0]),
+            [
+                0.999954600070331,
+                0.00004539786860886666,
+                0.000000002061060046209062
+            ]
+        );
+    }
+}
