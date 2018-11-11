4797f5cb66a6ee8afa71f3c46d47f8a9ba8dac9a
Takeru Ohta <phjgt308@gmail.com>
2018-11-11 18:04:42 +0900

    Add a matrix test

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,10 +1,10 @@
 use std::iter::Sum;
-use std::ops::{Add, Div, Mul, Sub};
+use std::ops::{Add, AddAssign, Div, Mul, Sub};
 
 // FIXME: optimize inner representation
 //        (e.g., `{ inner: Vec<T>, rows: usize, cols: usize }`)
-#[derive(Debug, PartialEq, Eq)]
-pub struct Matrix<T>(Vec<Vec<T>>);
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Matrix<T = f64>(Vec<Vec<T>>);
 impl<T> Matrix<T>
 where
     T: Default + Clone,
@@ -15,12 +15,10 @@ where
 }
 impl<T> Matrix<T>
 where
-    T: Default + Clone + Mul<Output = T> + Add + Sub + Div + Sum,
+    T: Default + Clone + Mul<Output = T> + Add + AddAssign + Sub + Div + Sum + std::fmt::Debug,
 {
     pub fn dot_product(&self, other: &Self) -> Self {
-        assert_eq!(self.rows(), other.columns());
         assert_eq!(self.columns(), other.rows());
-
         let mut out = Matrix::new(self.rows(), other.columns());
         for y in 0..self.rows() {
             for x in 0..other.columns() {
@@ -35,12 +33,23 @@ where
         out
     }
 
+    pub fn add(mut self, other: &Self) -> Self {
+        assert_eq!(self.rows(), other.rows());
+        assert_eq!(self.columns(), other.columns());
+        for y in 0..self.rows() {
+            for x in 0..self.columns() {
+                self.0[y][x] += other.0[y][x].clone();
+            }
+        }
+        self
+    }
+
     pub fn rows(&self) -> usize {
-        self.0.get(0).map_or(0, |x| x.len())
+        self.0.len()
     }
 
     pub fn columns(&self) -> usize {
-        self.0.len()
+        self.0.get(0).map_or(0, |x| x.len())
     }
 
     pub fn row(&self, i: usize) -> impl Iterator<Item = &T> {
@@ -55,6 +64,19 @@ where
 
     pub fn into_vec(self) -> Vec<Vec<T>> {
         self.0
+    }
+
+    pub fn map<F>(mut self, f: F) -> Self
+    where
+        F: Fn(&T) -> T,
+    {
+        for y in 0..self.rows() {
+            for x in 0..self.columns() {
+                let t = f(&self.0[y][x]);
+                self.0[y][x] = t;
+            }
+        }
+        self
     }
 }
 impl<T> From<Vec<Vec<T>>> for Matrix<T> {
@@ -81,5 +103,44 @@ mod tests {
             a.dot_product(&b).into_vec(),
             vec![vec![19, 22], vec![43, 50]]
         );
+    }
+
+    #[test]
+    fn section_3_4_3() {
+        use functions::activation::{identity, sigmoid};
+
+        struct Network {
+            w1: Matrix,
+            b1: Matrix,
+            w2: Matrix,
+            b2: Matrix,
+            w3: Matrix,
+            b3: Matrix,
+        }
+        impl Network {
+            fn forward(&self, input: &[f64]) -> Vec<f64> {
+                let x = Matrix::from(vec![Vec::from(input)]);
+                let a1 = x.dot_product(&self.w1).add(&self.b1);
+                let z1 = a1.map(|&v| sigmoid(v));
+                let a2 = z1.dot_product(&self.w2).add(&self.b2);
+                let z2 = a2.map(|&v| sigmoid(v));
+                let a3 = z2.dot_product(&self.w3).add(&self.b3);
+                let y = a3.map(|&v| identity(v));
+                y.into_vec().swap_remove(0)
+            }
+        }
+
+        let network = Network {
+            w1: Matrix::from(vec![vec![0.0, 0.3, 0.5], vec![0.2, 0.4, 0.6]]),
+            b1: Matrix::from(vec![vec![0.1, 0.2, 0.3]]),
+            w2: Matrix::from(vec![vec![0.1, 0.4], vec![0.2, 0.5], vec![0.3, 0.6]]),
+            b2: Matrix::from(vec![vec![0.1, 0.2]]),
+            w3: Matrix::from(vec![vec![0.1, 0.3], vec![0.2, 0.4]]),
+            b3: Matrix::from(vec![vec![0.1, 0.2]]),
+        };
+
+        let x = [1.0, 0.5];
+        let y = network.forward(&x);
+        assert_eq!(y, [0.3111600656569811, 0.673942886171143]);
     }
 }
